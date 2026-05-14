ab05686d67bc5ee8b96104c9ac3c0085f1c3655e
Tas <tasvss@gmail.com>
2026-05-14 20:28:06 +0200

    nyert valaki felkesz

--- script.js
+++ script.js
@@ -14,5 +14,25 @@ function kattintas(index) {
     }
 
 
+ellenorzes();
+}
+function ellenorzes() {
+    const nyeroMintak = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+        [0, 4, 8], [2, 4, 6]
+    ];
 
+    let nyertValaki = false;
+
+    for (let minta of nyeroMintak) {
+        let a = tabla[minta[0]];
+        let b = tabla[minta[1]];
+        let c = tabla[minta[2]];
+
+        if (a !== "" && a === b && b === c) {
+            nyertValaki = true;
+            break;
+        }
+    }
 }
